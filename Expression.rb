bc165f3aa38bf1abd6b4f75119d9c7d03d622c66
Alex-Karapetkov <karapeat@dukes.jmu.edu>
2024-05-02 18:31:36 -0400

    Revert "Added code for milestone 1"
    This reverts commit df41cc7db2e6477805b845d6041ebc139eeb1040.

--- Expression.rb
+++
@@ -1,9 +0,0 @@
-module Expression
-    def evaluate(environment)
-        raise NotImplementedError, "Subclass must implement the evaluate method."
-    end
-
-    def to_s
-        raise NotImplementedError, "Subclass must implement the to_s method."
-    end
-end
